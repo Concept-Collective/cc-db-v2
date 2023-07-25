612723593eb07a814e0ecdbcf7c283731648137a
Mr. K V <36492979+69u@users.noreply.github.com>
2023-07-25 20:26:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   INTERACTIONS: {
     SLASH: true, // Should the interactions be enabled
     CONTEXT: true, // Should contexts be enabled
-    GLOBAL: false, // Should the interactions be registered globally
+    GLOBAL: true, // Should the interactions be registered globally
     TEST_GUILD_ID: "807309391849062480", // Guild ID where the interactions should be registered. [** Test you commands here first **]
   },
   EMBED_COLORS: {
